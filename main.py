f15e7509c2c5cf27b3ff4e395a22ee1d2027e3de
Laweens <laweensfrancois@gmail.com>
2024-04-09 08:57:27 -0400

    finally

--- main.py
+++ main.py
@@ -1,61 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for, g
-from dynaconf import Dynaconf
-
-import pymysql
-import pymysql.cursors
-from pprint import pprint as print 
-import flask_login
+from flask import Flask, render_template, jsonify
+import sqlite3
 
 app = Flask(__name__)
 
-settings= Dynaconf(
-    settings_file=('settings.toml')
-)
+# Function to retrieve information from the database
+def get_information_from_database():
+    conn = sqlite3.connect('your_database.db')  # Replace 'your_database.db' with your database file
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM your_table')  # Replace 'your_table' with your table name
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
 
-class user:
-    is_authenticated = True 
-    is_anonymous = False
-    is_active = True
-    def __init__(self, id, username):
-        self.username = username 
-        self.id = id 
-    def get_id(self):
-        return str(self.id)
+@app.route('/get_information', methods=['GET'])
+def get_information():
+    information = get_information_from_database()
+    return jsonify(information)
 
-
-def connect_db():
-    return pymysql.connect(
-        database = "sports_aggregator",
-        user = "sjamesjr",
-        password = "250415031",
-        host = "10.100.33.60",
-        cursorclass=pymysql.cursors.DictCursor,
-        autocommit=True
-    )
-def get_db():
-    '''Opens a new database connection per request.'''        
-    if not hasattr(g, 'db'):
-        g.db = connect_db()
-    return g.db    
-
-@app.teardown_appcontext
-def close_db(error):
-    '''Closes the database connection at the end of request.'''    
-    if hasattr(g, 'db'):
-        g.db.close() 
-
-@app.route('/')
-
-@app.route('/feed')
-def post_feed():
-    cursor = get_db().cursor()
-    cursor.execute('SELECT * from `Games` ORDER BY `datetime`')
-    results = cursor.fetchall()
-    cursor.close()
-    return render_template("feed.html.jinja",post_list=results)
-
-def index():
-    cursor = get_db().cursor()
-    results = cursor.fetchall()
-    cursor.close()
-    return render_template("feed.html.jinja")
+if __name__ == '__main__':
+    app.run(debug=True)
